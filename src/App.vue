2d360a47a28b23d5992340bbff35874542ce4463
Maddie Gareth <maddiega@et-g6.kzn.21-school.ru>
2022-10-09 09:20:43 +0300

    feat: realise header block

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
-  <div>Hello Vue!</div>
+  <div class="app-container">
+    <header-block></header-block>
+    <!--    <add-todo-block></add-todo-block>-->
+    <!--    <todos-block></todos-block>-->
+  </div>
 </template>
+
+<script>
+import HeaderBlock from './components/HeaderBlock.vue';
+// import AddTodoBlock from './components/AddTodoBlock.vue';
+// import TodosBlock from './components/TodosBlock.vue';
+
+export default {
+  components: {
+    HeaderBlock,
+    // AddTodoBlock,
+    // TodosBlock,
+  },
+};
+</script>
+
+<style>
+.app-container {
+  max-width: 760px;
+  margin: 0 auto;
+  padding: 40px;
+}
+</style>
---
+++ src/components/HeaderBlock.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="title">Todo</h1>
+</template>
+
+<script></script>
+
+<style scoped>
+.title {
+  margin-bottom: 10px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+</style>
