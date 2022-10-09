4260f07cfd9f860cc48dfa6c3492754fcf9199f4
Maddie Gareth <maddiega@et-g6.kzn.21-school.ru>
2022-10-09 09:04:11 +0300

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import store from './store';
 
-import './assets/main.scss';
+import './assets/base.scss';
 
-createApp(App).mount('#app');
+createApp(App).use(store).mount('#app');
